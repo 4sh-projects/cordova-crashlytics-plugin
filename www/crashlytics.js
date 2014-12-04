5a4b69e25798c5712db1c6f760196a1b084f086d
fcamblor <frederic.camblor@4sh.fr>
2014-12-04 17:09:15 +0100

    Mapped every new methods + provided ripple mocking support out of the box

--- www/crashlytics.js
+++ www/crashlytics.js
@@ -4,8 +4,25 @@ var exec = require('cordova/exec');
 
 var Crashlytics = function(){
 
-    this.logException = function(msg) {
-        exec(null, null, "Crashlytics", "logException", [msg]);
+    var methods = [
+        'logException', 'log', 'setApplicationInstallationIdentifier',
+        'setBool', 'setDouble', 'setFloat', 'setInt', 'setLong', 'setString', 'setUserEmail',
+        'setUserIdentifier', 'setUserName'
+    ];
+
+    var execCall;
+    var rippleMock = (window.parent && window.parent.ripple);
+    if(rippleMock) {
+        console.warn("navigator.crashlytics not defined : considering you're in dev mode and mocking it !");
+        execCall = function(methodName, args){ console.log("[Crashlytics] Call to "+methodName+"("+args+")"); }
+    } else {
+        execCall = function(methodName, args){ exec(null, null, "Crashlytics", methodName, args); };
+    }
+
+    for(var i=0; i<methods.length; i++) {
+        this[methods[i]] = function(){
+            execCall(methods[i], arguments);
+        };
     }
 };
 
