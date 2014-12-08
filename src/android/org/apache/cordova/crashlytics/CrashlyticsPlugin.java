ab6dd58a158fa650403b0ac8515104e47474abea
fcamblor <frederic.camblor@4sh.fr>
2014-12-08 13:58:03 +0100

    Refactored CrashlyticsPlugin using enum instead of map

--- src/android/org/apache/cordova/crashlytics/CrashlyticsPlugin.java
+++ src/android/org/apache/cordova/crashlytics/CrashlyticsPlugin.java
@@ -5,9 +5,6 @@ import com.crashlytics.android.Crashlytics;
 import org.apache.cordova.*;
 import org.json.JSONException;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class CrashlyticsPlugin extends CordovaPlugin {
 
     @Override
@@ -16,17 +13,103 @@ public class CrashlyticsPlugin extends CordovaPlugin {
         Crashlytics.start((Context) cordova.getActivity());
     }
 
-    static interface CrashlyticsCallSite {
-        public void call(CordovaArgs args) throws JSONException;
-    }
+    private static enum BridgedMethods {
+        logError(1){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.log(args.getString(0));
+            }
+        },
+        // Kept for backward compatibility only ...
+        throwError(1){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.logException(new RuntimeException(args.getString(0)));
+            }
+        },
+        logException(1){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.logException(new RuntimeException(args.getString(0)));
+            }
+        },
+        log(1){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                if (argsLengthValid(3, args)) {
+                    Crashlytics.log(args.getInt(0), args.getString(1), args.getString(2));
+                } else {
+                    Crashlytics.log(args.getString(0));
+                }
+            }
+        },
+        setApplicationInstallationIdentifier(1){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.setApplicationInstallationIdentifier(args.getString(0));
+            }
+        },
+        setBool(2){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.setBool(args.getString(0), args.getBoolean(1));
+            }
+        },
+        setDouble(2){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.setDouble(args.getString(0), args.getDouble(1));
+            }
+        },
+        setFloat(2){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.setFloat(args.getString(0), Float.valueOf(args.getString(1)));
+            }
+        },
+        setInt(2){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.setInt(args.getString(0), args.getInt(1));
+            }
+        },
+        setLong(2){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.setLong(args.getString(0), args.getLong(1));
+            }
+        },
+        setString(2){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.setString(args.getString(0), args.getString(1));
+            }
+        },
+        setUserEmail(1){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.setUserEmail(args.getString(0));
+            }
+        },
+        setUserIdentifier(1){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.setUserIdentifier(args.getString(0));
+            }
+        },
+        setUserName(1){
+            @Override
+            public void call(CordovaArgs args) throws JSONException {
+                Crashlytics.setUserName(args.getString(0));
+            }
+        };
 
-    static class CrashlyticsCall {
         int minExpectedArgsLength;
-        CrashlyticsCallSite callSite;
-        public CrashlyticsCall(int minExpectedArgsLength, CrashlyticsCallSite callSite) {
+        BridgedMethods(int minExpectedArgsLength) {
             this.minExpectedArgsLength = minExpectedArgsLength;
-            this.callSite = callSite;
         }
+
+        public abstract void call(CordovaArgs args) throws JSONException;
 
         public static boolean argsLengthValid(int minExpectedArgsLenght, CordovaArgs args) throws JSONException {
             return (args != null
@@ -36,120 +119,31 @@ public class CrashlyticsPlugin extends CordovaPlugin {
         }
     }
 
-    public static final Map<String, CrashlyticsCall> CALL_SITES = new HashMap<String, CrashlyticsCall>(){{
-        this.put("logError", new CrashlyticsCall(1, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.log(args.getString(0));
-            }
-        }));
-        // Kept for backward compatibility only ...
-        this.put("throwError", new CrashlyticsCall(1, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.logException(new RuntimeException(args.getString(0)));
-            }
-        }));
-        this.put("logException", new CrashlyticsCall(1, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.logException(new RuntimeException(args.getString(0)));
-            }
-        }));
-        this.put("log", new CrashlyticsCall(1, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                if (CrashlyticsCall.argsLengthValid(3, args)) {
-                    Crashlytics.log(args.getInt(0), args.getString(1), args.getString(2));
-                } else {
-                    Crashlytics.log(args.getString(0));
-                }
-            }
-        }));
-        this.put("setApplicationInstallationIdentifier", new CrashlyticsCall(1, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.setApplicationInstallationIdentifier(args.getString(0));
-            }
-        }));
-        this.put("setBool", new CrashlyticsCall(2, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.setBool(args.getString(0), args.getBoolean(1));
-            }
-        }));
-        this.put("setDouble", new CrashlyticsCall(2, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.setDouble(args.getString(0), args.getDouble(1));
-            }
-        }));
-        this.put("setFloat", new CrashlyticsCall(2, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.setFloat(args.getString(0), Float.valueOf(args.getString(1)));
-            }
-        }));
-        this.put("setInt", new CrashlyticsCall(2, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.setInt(args.getString(0), args.getInt(1));
-            }
-        }));
-        this.put("setLong", new CrashlyticsCall(2, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.setLong(args.getString(0), args.getLong(1));
-            }
-        }));
-        this.put("setString", new CrashlyticsCall(2, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.setString(args.getString(0), args.getString(1));
-            }
-        }));
-        this.put("setUserEmail", new CrashlyticsCall(1, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.setUserEmail(args.getString(0));
-            }
-        }));
-        this.put("setUserIdentifier", new CrashlyticsCall(1, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.setUserIdentifier(args.getString(0));
-            }
-        }));
-        this.put("setUserName", new CrashlyticsCall(1, new CrashlyticsCallSite() {
-            @Override
-            public void call(CordovaArgs args) throws JSONException {
-                Crashlytics.setUserName(args.getString(0));
-            }
-        }));
-
-    }};
-
     @Override
     public boolean execute(String action, final CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
-        if(CALL_SITES.containsKey(action)) {
-            final CrashlyticsCall crashlyticsCall = CALL_SITES.get(action);
-            if(!CrashlyticsCall.argsLengthValid(crashlyticsCall.minExpectedArgsLength, args)) {
-                callbackContext.error(String.format("Unsatisfied min args length (expected=%s)", crashlyticsCall.minExpectedArgsLength));
+        try {
+            final BridgedMethods bridgedMethods = BridgedMethods.valueOf(action);
+            if (bridgedMethods != null) {
+                if (!BridgedMethods.argsLengthValid(bridgedMethods.minExpectedArgsLength, args)) {
+                    callbackContext.error(String.format("Unsatisfied min args length (expected=%s)", bridgedMethods.minExpectedArgsLength));
+                    return true;
+                }
+
+                cordova.getThreadPool().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            bridgedMethods.call(args);
+                            callbackContext.success();
+                        } catch (JSONException e) {
+                            callbackContext.error(e.getMessage());
+                        }
+                    }
+                });
                 return true;
             }
-
-            cordova.getThreadPool().execute(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        crashlyticsCall.callSite.call(args);
-                        callbackContext.success();
-                    } catch (JSONException e) {
-                        callbackContext.error(e.getMessage());
-                    }
-                }
-            });
-            return true;
+        }catch(IllegalArgumentException e) {
+            return false;
         }
 
         return false;
